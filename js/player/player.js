fc8040e34c5fc3b32c044529226af106eef4e002
Lonnie James <lj558231@gmail.com>
2020-08-27 12:33:27 +0200

    Add class player

---
+++ js/player/player.js
@@ -0,0 +1,36 @@
+class player {
+    constructor(id, column, row, weapon){
+        this.id = id;
+        this.column = column;
+        this.row = row;
+        this.weapon = weapon;
+        this.hp = 100;
+        this.defenseMode = fasle;
+        this.combat = false;
+        this.name = "joueur " + id;
+    }
+
+    move(){
+
+    }
+
+    switchWeapon(){
+
+    }
+
+    attack(){
+
+    }
+
+    defend(){
+
+    }
+
+    isAttacked(){
+
+    }
+
+    update(){
+        
+    }
+}
