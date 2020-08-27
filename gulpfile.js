dff0de7934f7597ae3a806753603c13d277bdce5
Lonnie James <lj558231@gmail.com>
2020-08-27 13:57:23 +0200

    add gulp dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,31 @@ const cleanCSS   = require("gulp-clean-css");
 const browerSync = require("browser-sync");
 const concat     = require("gulp-concat");
 
-// function style(){
-//   return gulp.src("./src/**/*.scss")
-//     .pipe(sass())
-//     .pipe(concat('style.css'))
-//     .pipe(gulp.dest('./app/css'));
-// }
+function style(){
+  return gulp.src("./src/**/*.scss")
+    .pipe(sass())
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./app/css'));
+}
 
-// function build(){
-//   return gulp.src("./src/**/*.scss")
-//     .pipe(sass())
-//     .pipe(concat('style.css'))
-//     .pipe(cleanCSS())
-//     .pipe(gulp.dest('./app/css'));
-// }
+function build(){
+  return gulp.src("./src/**/*.scss")
+    .pipe(sass())
+    .pipe(concat('style.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./app/css'));
+}
 
-// function watch(){
-//   browerSync.init({
-//     server: {
-//       baseDir : "./app"
-//     }
-//   });
+function watch(){
+  browerSync.init({
+    server: {
+      baseDir : "./app"
+    }
+  });
 //   gulp.watch("./src/**/*.scss", style);
 //   gulp.watch("./app/").on("change", browerSync.reload);
-// }
+}
 
-// exports.build = build;
-// exports.style = style;
-// exports.watch = watch;
+exports.build = build;
+exports.style = style;
+exports.watch = watch;
